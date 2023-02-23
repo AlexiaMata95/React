774151a9499ac8419605d2575a8aa7676043fd51
Alexia Mata <alexia.i.mata.perez@outlook.com>
2023-02-22 21:07:13 -0600

    Uso de estados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Hello from './Components/hello'
-import Padre from './Components/padre';
+import Padre from './Components/padre'
+import Counter from './Components/Counter';
 
 function App() {
   const handleClick = () =>{
@@ -9,24 +9,13 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
         <Hello name='Alo'/>
         <Padre/>
-        <button onClick={()=>console.log('Hiciste click!')}>Click me!</button>
-      </header>
+        <Counter/>
+        <button onClick={()=>handleClick}>Click me!</button>
     </div>
+    
   );
 }
 
